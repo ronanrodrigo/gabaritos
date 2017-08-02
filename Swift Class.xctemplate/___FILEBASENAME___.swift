dddeb9666d2659c79e9b56760783cedd119524e3
Ronan Rodrigo Nunes <ronan.nunes@me.com>
2017-08-02 11:21:42 -0300

    Create first gabaritos

---
+++ Swift Class.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,4 @@
+import Foundation
+
+class ___FILEBASENAMEASIDENTIFIER___ {
+}
---
+++ Swift Quick.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,22 @@
+import QuickLook
+import Nimble
+
+class ___FILEBASENAMEASIDENTIFIER___Spec: QuickSpec {
+
+    override func spec() {
+        super.spect()
+
+        describe("Given ") {
+
+            context("when ") {
+
+                it("then ") {
+                }
+
+            }
+
+        }
+
+    }
+
+}
